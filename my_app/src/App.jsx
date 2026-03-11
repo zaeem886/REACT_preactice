4043028b5f9b321638b94ac182e5c04bc120e403
zaeem886 <zaeemch317@gmail.com>
2026-03-12 03:42:15 +0500

    Array of objects practice

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -1,69 +1,149 @@
+// import { useState } from 'react';
+
+// export default function Form() {
+//   const [answer, setAnswer] = useState('');
+//   const [error, setError] = useState(null);
+//   const [status, setStatus] = useState('typing');
+
+//   if (status === 'success') {
+//     return <h1>That's right!</h1>
+//   }
+
+//   async function handleSubmit(e) {
+//     e.preventDefault();
+//     setStatus('submitting');
+//     try {
+//       await submitForm(answer);
+//       setStatus('success');
+//     } catch (err) {
+//       setStatus('typing');
+//       setError(err);
+//     }
+//   }
+
+//   function handleTextareaChange(e) {
+//     setAnswer(e.target.value);
+//   }
+
+//   return (
+//     <>
+//       <h2>City quiz</h2>
+//       <p>
+//         In which city is there a billboard that turns air into drinkable water?
+//       </p>
+//       <form onSubmit={handleSubmit}>
+//         <textarea
+//           value={answer}
+//           onChange={handleTextareaChange}
+//           disabled={status === 'submitting'}
+//         />
+//         <br />
+//         <button disabled={
+//           answer.length === 0 ||
+//           status === 'submitting'
+//         }>
+//           Submit
+//         </button>
+//         {error !== null &&
+//           <p className="Error">
+//             {error.message}
+//           </p>
+//         }
+//       </form>
+//     </>
+//   );
+// }
+
+// function submitForm(answer) {
+//   // Pretend it's hitting the network.
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let shouldError = answer.toLowerCase() !== 'lima'
+//       if (shouldError) {
+//         reject(new Error('Good guess but a wrong answer. Try again!'));
+//       } else {
+//         resolve();
+//       }
+//     }, 1500);
+//   });
+// }
+import './App.css';
 import { useState } from 'react';
 
-export default function Form() {
-  const [answer, setAnswer] = useState('');
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState('typing');
+function MyComponent () {
+const [cars,setCars]=useState([]);
+const [carYear,setCarYear]=useState(new Date().getFullYear());
+const [carMake,setCarMake]=useState('');
+const [carModel,setCarModel]=useState('');
 
-  if (status === 'success') {
-    return <h1>That's right!</h1>
+
+function handleAddCar(){
+  const year = Number(carYear);
+  const currentYear = new Date().getFullYear();
+
+  if(year < 1886 || year > currentYear){
+    alert("Please enter a valid year between 1886 and the current year.");
+    return;
   }
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus('submitting');
-    try {
-      await submitForm(answer);
-      setStatus('success');
-    } catch (err) {
-      setStatus('typing');
-      setError(err);
-    }
-  }
+  const newCar = {
+    year: carYear,
+    make: carMake,
+    model: carModel
+  };
 
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
-  }
+  setCars(c => [...c, newCar]);
 
-  return (
-    <>
-      <h2>City quiz</h2>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          disabled={status === 'submitting'}
-        />
-        <br />
-        <button disabled={
-          answer.length === 0 ||
-          status === 'submitting'
-        }>
-          Submit
-        </button>
-        {error !== null &&
-          <p className="Error">
-            {error.message}
-          </p>
-        }
-      </form>
-    </>
-  );
+  setCarYear(new Date().getFullYear());
+  setCarMake('');
+  setCarModel('');
+}
+function handleRemoveCar(index){
+
+  setCars(c=>c.filter((_,i)=> i!=index));
+
+
+}
+function handleCarYearChange(e){
+  setCarYear(e.target.value);
+
+
+  
+}
+function handleCarMakeChange(e){
+  
+setCarMake(e.target.value);
+}
+function handleCarModelChange(e){
+  
+setCarModel(e.target.value);  
 }
 
-function submitForm(answer) {
-  // Pretend it's hitting the network.
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== 'lima'
-      if (shouldError) {
-        reject(new Error('Good guess but a wrong answer. Try again!'));
-      } else {
-        resolve();
-      }
-    }, 1500);
-  });
+
+return(<div>
+
+<h2>Car List</h2>
+
+<ul>
+  {
+    cars.map((car,index) => 
+      <li  className="car-item" key={index} onClick={() => handleRemoveCar(index)}>
+        {car.year} {car.make} {car.model}
+      </li>
+    )
+  }
+
+</ul>
+
+
+<input type="number" value={carYear} onChange={handleCarYearChange} /><br />
+<input type="text" value={carMake} onChange={handleCarMakeChange} placeholder='Enter car make' /><br />
+<input type="text" value={carModel} onChange={handleCarModelChange} placeholder='enter car model' /><br /><br /> 
+<button onClick={handleAddCar}>Add Car</button>
+
+</div>)
+
+
 }
+
+export default MyComponent;
